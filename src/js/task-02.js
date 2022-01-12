33b838eab0e6a306328dbbf3238c77fba3c9c509
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-12 20:42:27 +0200

    completed task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsArr = [];
+
+ingredients.forEach((element) => {
+  const ingredientsItems = document.createElement("li");
+  ingredientsItems.classList.add("item");
+
+  ingredientsItems.textContent = element;
+  ingredientsArr.push(ingredientsItems);
+});
+
+ingredientsList.append(...ingredientsArr);
