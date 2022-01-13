fb5558e684b1596a0841c3b515bac8b273ad3bf3
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-13 20:31:29 +0200

    fixed styles and completed task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,5 +2,5 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-  gitoutput.textContent = event.currentTarget.value;
+  output.textContent = event.currentTarget.value;
 });
