24a440bf8934075a2850a3ca8f930e56c28fcad9
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-14 21:07:14 +0200

    completed task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector(".color");
+const buttonChangeColor = document.querySelector("button");
+const body = document.body;
+
+buttonChangeColor.addEventListener("click", () => {
+  colorValue.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorValue.textContent;
+});
